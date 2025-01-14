39528e0603235d69bf73dc3a5609ec421e84f1fd
Jesus Sanchez <jhsanro@gmail.com>
2025-01-14 13:40:38 -0500

    Clean layers

--- src/lib/presentation/views/SmartspecsView.tsx
+++ src/lib/presentation/views/SmartspecsView.tsx
@@ -2,7 +2,6 @@
 import { useEffect } from "react";
 import { Card, Row, Col, Tag, Button, Skeleton, Empty } from "antd";
 import { ReloadOutlined } from "@ant-design/icons";
-
 import {
   Dropdown,
   DropdownItem,
